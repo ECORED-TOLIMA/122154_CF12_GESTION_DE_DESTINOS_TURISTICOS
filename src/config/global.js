ab6b25625a68562de707c0ad8f11c2d528bcfda2
veimarcm <vcelis@sena.edu.co>
2025-02-18 14:39:51 -0500

    Reapply "Update global.js"
    Act prueba

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,7 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE _DESCRIPCIÓN',
+    componenteFormativo: 'NOMBRE_DEL_COMPONENTE_FORMATIVO',
+    descripcionCurso: 'BREVE_DESCRIPCIÓN',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
